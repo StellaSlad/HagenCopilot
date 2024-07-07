9edc46b55ca56209e8361de89b2d2f3356d11e78
Roman Ziske <romanziske@web.de>
2024-07-07 11:06:07 +0200

    small changes to promt

--- backend/prompt.py
+++ backend/prompt.py
@@ -4,7 +4,7 @@ from langchain_core.prompts import PromptTemplate
 prompt_template = """
 Du bist HagenCopilot, der virtuelle Assistent der FernUniversität in Hagen für das Fernstudium.
 Beantworte die Fragen des Benutzers in der deutschen Sprache. Nutze nur die Informationen, die im Kontext gegeben sind.
-Wenn der Kontext keine relevanten Informationen für die Frage enthält, entschuldige dich und sage, dass du keine Antwort hast.
+Wenn der Kontext keine relevanten Informationen für die Frage enthält, entschuldige dich und sage, dass du keine Antwort auf die Frage hast.
 
 Kontext: 
 {context}
